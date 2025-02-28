929aec84af37e46e397dd2d9b69cd4dc98a70de0
Aravind S <aravindprime123@gmail.com>
2025-02-28 07:35:18 +0530

    Fixed build issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Topbar from "./components/topbar/Topbar";
 import Menu from "./components/menu/Menu";
 import "./app.scss";
 import Section from "./Section";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   const [menu, setMenu] = useState(false);
 
   return (
+
+    <BrowserRouter basename="/First-Portfolio">
+
     <div className='App'>
       <Topbar menu={menu} setMenu={setMenu} />
       <Menu menu={menu} setMenu={setMenu} />
@@ -15,6 +19,8 @@ function App() {
         <Section menu={menu} setMenu={setMenu} />
       </div>
     </div>
+
+    </BrowserRouter>
   );
 }
 
